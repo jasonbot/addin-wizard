4b66c81bdbbde2e461d96480b59299b8efa3d591
"Jason Scheirer" <jason.scheirer@gmail.com>
2011-03-14 16:37:50 -0700

    Real backup ignoring

--- packaging/makeaddin.py
+++ packaging/makeaddin.py
@@ -1,10 +1,16 @@
 import os
+import re
 import zipfile
 
 current_path = os.path.dirname(os.path.abspath(__file__))
 
 out_zip_name = os.path.join(current_path, 
                             os.path.basename(current_path) + ".esriaddin")
+
+BACKUP_FILE_PATTERN = re.compile(".*_addin_[0-9]+[.]py$", re.IGNORECASE)
+
+def looks_like_a_backup(filename):
+    return bool(BACKUP_FILE_PATTERN.match(filename))
 
 zip_file = zipfile.ZipFile(out_zip_name, 'w')
 for filename in ('config.xml', 'README.txt', 'makeaddin.py'):
@@ -14,7 +20,7 @@ for directory in dirs_to_add:
     for (path, dirs, files) in os.walk(os.path.join(current_path, directory)):
         archive_path = os.path.relpath(path, current_path)
         found_file = False
-        for file in (f for f in files if not f.lower().endswith('_backup.py')):
+        for file in (f for f in files if not looks_like_a_backup(f)):
             archive_file = os.path.join(archive_path, file)
             print archive_file
             zip_file.write(os.path.join(path, file), archive_file)

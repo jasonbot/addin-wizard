5f417512ef3afa9c36e642b1b7794c3b7e22e547
"Jason Scheirer" <jason.scheirer@gmail.com>
2013-06-06 16:53:10 -0700

    Whitespace

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from distutils.core import setup
 import py2exe
 import glob
  
-setup(windows=[{'script': 'addin_assistant.pyw', 'icon_resources': [(1, "images\\AddInDesktop.ico")]}],
+setup(windows=[
+                {'script': 'addin_assistant.pyw',
+                 'icon_resources': [(1, "images\\AddInDesktop.ico")]
+                }
+              ],
       options={ "py2exe": { "dll_excludes": ["MSVCP90.dll"] }},
       data_files=[('images', glob.glob("images\\*.png") + 
                              glob.glob("images\\*.ico")),
